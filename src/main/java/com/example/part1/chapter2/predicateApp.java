fbdfcddcfc759f3a71ebddfa7e9accdb25fce216
HyuneyBee <gsh2448@gmail.com>
2023-10-18 23:52:58 +0900

    익명클래스 및 람다 표현식

--- src/main/java/com/example/part1/chapter2/predicateApp.java
+++ src/main/java/com/example/part1/chapter2/predicateApp.java
@@ -8,6 +8,16 @@ public class predicateApp {
         List<Apple> inventory = Apple.generateApples(10);
 
         List<Apple> greenApples = filterApples(inventory, new AppleGreenColorPredicate());
+
+        // 익명클래스
+        List<Apple> redApples = filterApples(inventory, new ApplePredicate() {
+            public boolean test(Apple apple) {
+                return Color.RED.equals(apple.getColor());
+            }
+        });
+
+        // 람다 표현식
+        List<Apple> heavyApples = filterApples(inventory, apple -> apple.getWeight() > 150);
     }
 
     // 함수형 인터페이스 활용한 필터 방법
