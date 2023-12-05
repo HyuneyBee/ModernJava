6f887a7e07bb253872decea571f76da5857d0a0a
HyuneyBee <gsh2448@gmail.com>
2023-12-05 22:47:10 +0900

    7.1 완료

--- src/main/java/com/example/part2/chapter7/Parallel.java
+++ src/main/java/com/example/part2/chapter7/Parallel.java
@@ -1,5 +1,6 @@
 package com.example.part2.chapter7;
 
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 public class Parallel {
@@ -15,6 +16,13 @@ public class Parallel {
     private static long parallelSum(long n){
         return Stream.iterate(1L, i -> i + 1)
             .limit(n)
+            .parallel()
+            .reduce(0L, Long::sum);
+    }
+
+    // 올바른 자료구조를 선택하여야 병렬 실행도 최적의 성능을 발휘
+    public static long parallelRangedSum(long n){
+        return LongStream.rangeClosed(1, n)
             .parallel()
             .reduce(0L, Long::sum);
     }
